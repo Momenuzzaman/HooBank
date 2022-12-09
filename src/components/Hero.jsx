5be24a663688dda4d8f939f659d867fc26ca58fb
mdemon420 <mdeamon4@gmail.com>
2022-12-09 16:23:25 +0600

    generation add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,35 @@ import styles from "../style";
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    <h1>Hero</h1>
+    <div
+      className={`flex-1 ${styles.flexStart} 
+    flex-col xl:px-0 sm:px:16 px-6`}
+    >
+      <div
+        className="flex flex-row items-center py-[6px] px-4 
+      bg-discount-gradient rounded-[10px] mb-2"
+      >
+        <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
+        <p className={`${styles.paragraph} ml-2`}>
+          <span className="text-white">20%</span> Discount for{" "}
+          <span className="text-white"> 1 Month</span> Account
+        </p>
+      </div>
+      <div
+        className="flex flex-row justify-between
+      items-center w-full"
+      >
+        <h1
+          className="flex-1 font-poppins font-semibold ss:text-[72px] text=[52]
+        text-white ss:leading-[100px] leading-[75px]"
+        >
+          The next <br className="sm:block hidden" />{" "}
+          <span className="text-gradient">Generation</span>{" "}
+          <br className="sm:block hidden" />
+          Payment Method
+        </h1>
+      </div>
+    </div>
   </section>
 );
 export default Hero;
