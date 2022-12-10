ff431be570caf4aa905bc583878ab766e3214c7b
mdemon420 <mdeamon4@gmail.com>
2022-12-11 01:51:23 +0600

    cardDeal complete

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ const Button = ({ style }) => {
       type="button"
       className={`py-4 px-6 bg-blue-gradient
       font-poppins font-medium text-[18px]
-      text-primary outline-none ${style}`}
+      text-primary outline-none ${style} rounded-[10px]`}
     >
       Get Started
     </button>
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
   return (
-    <div>
-      <h1>CardDeal</h1>
-    </div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className="sm:block hidden" />
+          in few easy steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Arcu tortor, purus in mattis at sed integer fauscibus. Aliquet quis
+          aliquet eget mauris tortor.c Aliquet ultrices ac,ametau.
+        </p>
+        <Button style="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
   );
 };
 
