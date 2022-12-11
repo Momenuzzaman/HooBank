05458ddfd5bc5f766e0f7b6215687fca4260fce7
mdemon420 <mdeamon4@gmail.com>
2022-12-11 11:56:27 +0600

    start feedBack card

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import { feedback } from "../constants";
+import Feedback from "./Feedback";
+import styles from "../style";
 
 const Testimonials = () => {
   return (
-    <div>
-      <h1>Testimonials</h1>
-    </div>
+    <section
+      className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}
+    >
+      <div />
+      <div
+        className="w-full flex justify-between
+      items-center md:flex-row flex-col sm:mb-16 
+      mb-6 relative z-[1]"
+      >
+        <h1 className={styles.heading2}>
+          What people are <br className="sm:block hidden" />
+          saying about us
+        </h1>
+        <div className="w-full md:mt-0 mt-6">
+          <p className={`${styles.paragraph} max-w-[450px] text-left`}>
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+      <div
+        className="flex flex-wrap sm:justify-start
+        justify-center w-full feedback-container relative z-[1]"
+      >
+        {}
+      </div>
+    </section>
   );
 };
 
